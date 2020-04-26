85bfda36b8abdcb1e6223d901f9bcec0dc9428fa
jlpunsalan <jem.punsalan@yahoo.com>
2020-04-26 22:35:35 +0200

    Updates

---
+++ precache-manifest.0a79f2aaab5413bec503e2c25a1ada6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0da456769b30f7a17889384a53dccfd",
+    "url": "/project-scheduler-app/index.html"
+  },
+  {
+    "revision": "d29ad7a7cb8af222e6ac",
+    "url": "/project-scheduler-app/static/css/2.e9f63770.chunk.css"
+  },
+  {
+    "revision": "9c6b2da8a55653a67af8",
+    "url": "/project-scheduler-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d29ad7a7cb8af222e6ac",
+    "url": "/project-scheduler-app/static/js/2.f2895814.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/project-scheduler-app/static/js/2.f2895814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6b2da8a55653a67af8",
+    "url": "/project-scheduler-app/static/js/main.1319c730.chunk.js"
+  },
+  {
+    "revision": "817737783d5eac780671",
+    "url": "/project-scheduler-app/static/js/runtime-main.b084853b.js"
+  }
+]);
